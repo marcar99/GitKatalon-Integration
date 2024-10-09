8dc95cd376d48c2a0f368b17c3ad1d1af87ee011
marcarter <marcarter@CAW2104832.ca.deloitte.com>
2024-10-09 06:53:29 -0700

    Committing tc and suite

---
+++ Test Suites/Smoke Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cef92d77-0a9e-4cf5-a87e-1f51576d2f60</testSuiteGuid>
+</TestSuiteEntity>
